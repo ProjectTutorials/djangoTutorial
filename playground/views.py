3d51d1c970c6e852c5503bfed61f836432a93b2b
Pratik Kabade <pratikkabade@hotmail.com>
2022-08-28 00:21:41 +0530

    Added views

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def say_hello(request):
+    return HttpResponse("Hello World")
+
+
+def hello_page(request):
+    return render(request, 'helloPage.html', {'name': 'Django'})
